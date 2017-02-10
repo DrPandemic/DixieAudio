9f0da1ffa62300b1b751149dec63919a7dbef7ef
Marie-Eve Dubé <mariedube2@gmail.com>
2017-02-10 13:30:07 -0500

    adding basic test

---
+++ test/TempTests.cpp
@@ -0,0 +1,22 @@
+//
+// Created by bugz on 10/02/17.
+//
+#include "gtest/gtest.h"
+
+TEST(basic_test, basic_test_int) {
+  int i = 3;
+  int j = 3;
+
+  EXPECT_EQ(i, 3);
+  EXPECT_EQ(j, 3);
+  EXPECT_EQ(i, j);
+}
+
+TEST(basic_test, basic_test_int_fail) {
+  int i = 3;
+  int j = 4;
+
+  EXPECT_EQ(i, 3);
+  EXPECT_EQ(j, 3);
+  EXPECT_EQ(i, j);
+}
--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,12 +1,8 @@
-#include <limits.h>
-#include <iostream>
 #include "gtest/gtest.h"
 
-using namespace std;
+#include "TempTests.cpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
-
-  cout << "Hello yeh" << endl;
   return RUN_ALL_TESTS();
 }
