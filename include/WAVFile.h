39d33c54bd70cd335da2772af79ee09c129ef995
DrPandemic <bipbip500@gmail.com>
2017-03-15 15:43:20 -0400

    Adds WAVHeader

--- include/WAVFile.h
+++ include/WAVFile.h
@@ -1,6 +1,29 @@
 #ifndef DIXIEAUDIO_WAVFILE_H
 #define DIXIEAUDIO_WAVFILE_H
 
+#import "AudioFile.h"
+#import <istream>
+#import <vector>
+
 class WAVFile : public AudioFile {};
+struct WAVHeader : public AudioHeader {
+  using string = std::string;
+  using byte = uint8_t;
+  using size = uint32_t;
+
+  // 4 bytes
+  size file_size;
+  // 4 bytes
+  // 4 bytes
+  size format_data_length;
+  uint16_t format_type;
+  uint16_t number_of_channels;
+  size sample_rate;
+  // 4 bytes
+  uint16_t channel_type;
+  uint16_t bits_pet_sample;
+  // 4 bytes
+  size data_size;
+};
 
 #endif // DIXIEAUDIO_WAVFILE_H
